9e0b30db06391b1ab770001002d98ff2aec132e6
tigre <tigrenok.77-98@mail.ru>
2026-04-17 20:36:20 +0400

    плюс ветка

---
+++ src/test/java/config/CredentialsConfig.java
@@ -0,0 +1,7 @@
+package config;
+import org.aeonbits.owner.Config;
+@Config.Sources("classpath:config/credentials.propertise")
+public interface CredentialsConfig extends Config {
+    String login();
+    String password();
+}
---
+++ src/test/java/properties/OwnerTest.java
@@ -0,0 +1,26 @@
+package properties;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+public class OwnerTest {
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+
+
+    @Test
+    @Tag("owner")
+    void someTest(){
+        String login=config.login();
+        String password=config.password();
+        System.out.println("Login: " + login);
+        System.out.println("Password: " + password);
+        String message = format("I logged in as %s with password %s", login, password);
+        System.out.println("Message " + message);
+    }
+}
